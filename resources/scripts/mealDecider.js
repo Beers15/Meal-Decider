dcc5fd0680df3aae4d4907fd6f116fbb947127c0
Beers15 <alexbeerz@yahoo.com>
2021-06-22 15:39:52 -0700

    Finish eventlisteners for all meal decider choice figures. Finish programming the transition between questions while storing user choices.

--- resources/scripts/mealDecider.js
+++ resources/scripts/mealDecider.js
@@ -1,15 +1,29 @@
-const startButton = document.querySelector('#pizza-btn');
 const mealDecider = document.querySelector('#main-container');
 const contents = document.querySelector('#main-content');
 
-const bfastImg =  makeImage("resources/imgs/breakfast.png", "breakfast");
+//initialize question choice figures
+const bfastImg = makeImage("resources/imgs/breakfast.png", "breakfast");
 const bfFig = makeFigure(bfastImg, "Breakfast");
-
-const lunchImg =  makeImage("resources/imgs/lunch.png", "lunch");
+const lunchImg = makeImage("resources/imgs/lunch.png", "lunch");
 const lunchFig = makeFigure(lunchImg, "Lunch");
-
-const dinImg =  makeImage("resources/imgs/dinner.png", "dinner");
+const dinImg = makeImage("resources/imgs/dinner.png", "dinner");
 const dinFig = makeFigure(dinImg, "Dinner");
+const vegImg = makeImage("resources/imgs/vegitarian.png", "vegitarian");
+const vegFig = makeFigure(vegImg, "Yes");
+const meatImg = makeImage("resources/imgs/meat.png", "meat");
+const meatFig = makeFigure(meatImg, "No");
+const hotImg = makeImage("resources/imgs/hot.png", "hot");
+const hotFig = makeFigure(hotImg, "Yes");
+const notHotImg = makeImage("resources/imgs/not_hot.png", "not hot");
+const notHotFig = makeFigure(notHotImg, "No");
+const empanadaImg = makeImage("resources/imgs/empanada.png", "empanada");
+const empanadaFig = makeFigure(empanadaImg, "South American");
+const pastaImg = makeImage("resources/imgs/pasta.png", "pasta");
+const pastaFig = makeFigure(pastaImg, "European");
+const sushiImg = makeImage("resources/imgs/sushi.png", "hot");
+const sushiFig = makeFigure(sushiImg, "Asian");
+const hotdogImg = makeImage("resources/imgs/hotdog.png", "hotdog");
+const hotdogFig = makeFigure(hotdogImg, "North American");
 
 function makeImage(src, alt) {
   let img = document.createElement('img');
@@ -33,12 +47,10 @@ function makeFigure(img, caption) {
 }
 
 function makeQuestion(question, answers) {
-  const q = document.createElement('h2');
-  q.textContent = question;
+  document.querySelector('.card-title').innerText = question;
 
   const choices = document.createElement('div');
   choices.className = "question-choices";
-  choices.appendChild(q);
 
   for(let answer of answers) {
     choices.appendChild(answer);
@@ -47,24 +59,126 @@ function makeQuestion(question, answers) {
   return choices;
 }
 
-startButton.addEventListener('click', function () {
-  sessionStorage.clear();
+function endAnimation() {
+  let endAnimationImg = document.createElement('img');
+  endAnimationImg.setAttribute('id', 'end-pizza-icon');
+  endAnimationImg.alt = 'pizza';
+  endAnimationImg.src = 'resources/imgs/pizza.png'
+  
+  let choices = document.querySelector('.question-choices');
+  choices.parentNode.replaceChild(endAnimationImg, choices);
 
-  const q = "What meal of the day are we talking about?"
-  mealDecider.replaceChild(makeQuestion(q, [bfFig, lunchFig, dinFig]), contents);
+  document.querySelector('.card-title').innerText = "Thinking of a meal that fits those criteria...";
+
+  endAnimationImg.addEventListener('animationend', function() {
+    document.querySelector('.card-title').innerText = "Your meal choice is:";
+
+    let result = document.createElement('h3');
+    //temp
+    result.innerText = "FOOD RESULT HERE";
+    let restartBtn = document.createElement('button');
+    restartBtn.innerText = "Start Over";
+    restartBtn.addEventListener('click', function () {
+      window.location.reload();
+    })
+
+    endAnimationImg.parentNode.replaceChild(result, endAnimationImg);
+
+    result.parentNode.appendChild(restartBtn);
+  });
+}
+
+//event listeners for all choice buttons in meal decider
+document.querySelector('#pizza-btn').addEventListener('click', function () {
+  localStorage.clear();
+
+  mealDecider.replaceChild(makeQuestion("What Meal of the day will you be eating?", [bfFig, lunchFig, dinFig]), contents);
 })
 
 bfFig.addEventListener('click', function() {
   console.log("User chose breakfast")
-  sessionStorage.setItem('breakfast', true);
+  localStorage.setItem('breakfast', true);
+
+  let choices = document.querySelector('.question-choices');
+  choices.parentNode.replaceChild(makeQuestion("Would you prefer only vegitarian options?", [vegFig, meatFig]), choices);
 })
 
 lunchFig.addEventListener('click', function() {
   console.log("User chose lunch")
-  sessionStorage.setItem('lunch', true);
+  localStorage.setItem('lunch', true);
+
+  let choices = document.querySelector('.question-choices');
+  choices.parentNode.replaceChild(makeQuestion("Would you prefer only vegitarian options?", [vegFig, meatFig]), choices);
 })
 
 dinFig.addEventListener('click', function() {
   console.log("User chose dinner");
-  sessionStorage.setItem('dinner', true);
+  localStorage.setItem('dinner', true);
+
+  let choices = document.querySelector('.question-choices');
+  choices.parentNode.replaceChild(makeQuestion("Would you prefer only vegitarian options?", [vegFig, meatFig]), choices);
+})
+
+vegFig.addEventListener('click', function() {
+  console.log("User chose vegitarian meal");
+  localStorage.setItem('vegitarian', true);
+
+  let choices = document.querySelector('.question-choices');
+  choices.parentNode.replaceChild(makeQuestion("Do we want a hot meal today?", [hotFig, notHotFig]), choices);
+})
+
+meatFig.addEventListener('click', function() {
+  console.log("User chose not vegitarian meal");
+  localStorage.setItem('vegitarian', false);
+
+  let choices = document.querySelector('.question-choices');
+  choices.parentNode.replaceChild(makeQuestion("Do we want a hot meal today?", [hotFig, notHotFig]), choices);
+})
+
+hotFig.addEventListener('click', function() {
+  console.log("User chose hot meal");
+  localStorage.setItem('hot', true);
+
+  let choices = document.querySelector('.question-choices');
+  choices.parentNode.replaceChild(makeQuestion("Generally speaking, what kind of food are you thinking of having?", [hotdogFig, empanadaFig, sushiFig, pastaFig]), choices);
+})
+
+notHotFig.addEventListener('click', function() {
+  console.log("User chose not hot meal");
+  localStorage.setItem('hot', false);
+
+  let choices = document.querySelector('.question-choices');
+  choices.parentNode.replaceChild(makeQuestion("Generally speaking, what kind of food are you thinking of having?", [hotdogFig, empanadaFig, sushiFig, pastaFig]), choices);
+})
+
+hotdogFig.addEventListener('click', function() {
+  console.log("User chose North American meal");
+  localStorage.setItem('NAmerica', true);
+
+  endAnimation();
+  console.log(localStorage);
+})
+
+empanadaFig.addEventListener('click', function() {
+  console.log("User chose South American meal");
+  localStorage.setItem('SAmerica', true);
+
+  endAnimation();
+  console.log(localStorage);
+})
+
+sushiFig.addEventListener('click', function() {
+  console.log("User chose Asian meal");
+  localStorage.setItem('Asian', true);
+
+  endAnimation();
+  console.log(localStorage);
+})
+
+pastaFig.addEventListener('click', function() {
+  console.log("User chose European meal");
+  localStorage.setItem('European', true);
+
+  endAnimation();
+  console.log(localStorage);
 })
